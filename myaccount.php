fb9739b1f218642e69a2b78d1ff84c58ae24f1d7
James Johnson <jamesjohnsonw@gmail.com>
2017-05-04 14:05:12 +0100

    NOT WORKING MyAccount changes
    Should be 2 white boxes on a white background. Not sure what I've done
    wrong here.

--- myaccount.php
+++ myaccount.php
@@ -2,30 +2,24 @@
     $page = 'My Account';
     include('includes/header.php')
 ?>
-        <div class='profile-picture'>
-        </div>
 
         <div class='myaccount'>
-          <h3 class="myaccount-title">Hi, <?php echo $_SESSION['firstName']?>!</h3>
-
-          <div class="myaccount-content">
-            <p>This page will display information about the user and give them the ability to change certain items.</p>
+            <div class='editprofile-box'>
+                <div class="change-password">
+                    <form action="updateaccount.php" method="post">
+                        <input type="password" placeholder="Old Password" name="oldPassword"><br>
+                        <input type="password" placeholder="New Password" name="newPassword"><br>
+                        <input type="password" placeholder="Confrim New Password" name="confirmNewPassword"><br>
+                        <input type="submit" name="submit" value="Change Password">
+                    </form>
+                </div>
+            </div>
 
             <div class='editprofile-box'>
 
             </div>
-
-            <div class="change-password">
-                <form action="updateaccount.php" method="post">
-                    <input type="password" placeholder="Old Password" name="oldPassword"><br>
-                    <input type="password" placeholder="New Password" name="newPassword"><br>
-                    <input type="password" placeholder="Confrim New Password" name="confirmNewPassword"><br>
-                    <input type="submit" name="submit" value="Change Password">
-                </form>
-            </div>
           </div>
 
-      </div>
     </main>
 </body>
 </html>
