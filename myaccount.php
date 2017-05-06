868ad9be8f47a5dbfc1e41257e57f706411291bf
James Johnson <jamesjohnsonw@gmail.com>
2017-05-06 16:09:22 +0100

    Test commit

--- myaccount.php
+++ myaccount.php
@@ -3,18 +3,8 @@
     include('includes/header.php')
 ?>
 
-        <div class='myaccount'>
-            <div class='editprofile-box'>
-                <div class="change-password">
-                    <form action="updateaccount.php" method="post">
-                        <input type="password" placeholder="Old Password" name="oldPassword"><br>
-                        <input type="password" placeholder="New Password" name="newPassword"><br>
-                        <input type="password" placeholder="Confrim New Password" name="confirmNewPassword"><br>
-                        <input type="submit" name="submit" value="Change Password">
-                    </form>
-                </div>
-            </div>
-          </div>
+        <div class="myaccount">
+        </div>
 
     </main>
 </body>
