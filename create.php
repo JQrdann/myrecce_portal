b1b2a74b524d5177605fc2210aa1808c0ffe5f7a
JQrdan <solfonictoast@gmail.com>
2017-07-16 21:55:52 +0100

    pls ignore the jpg

---
+++ create.php
@@ -0,0 +1,57 @@
+<?php
+    $page = 'Create a Recce';
+    include('includes/header.php')
+?>
+
+  <style>
+    main{
+      background-color: #f4f4f4 !important;
+      height: 100vh !important;
+    }
+  </style>
+
+  <!-- Progress markers on the side -->
+  <div id='create-progress'>
+    <!-- edit number of these depending on final creation process -->
+    <div class='progress-marker'></div>
+    <div class='progress-marker'></div>
+    <div class='progress-marker'></div>
+    <div class='progress-marker'></div>
+    <div class='progress-marker'></div>
+  </div>
+
+  <div id='create-modal'>
+    <!-- variable content -->
+  </div>
+
+  <section class='create-section'>
+
+  </section>
+
+  <section class='create-section'>
+
+  </section>
+
+  <section class='create-section'>
+
+  </section>
+
+  <section class='create-section'>
+
+  </section>
+
+  <section class='create-section'>
+
+  </section>
+
+  <div id='create-summary'>
+    <div id='create-publish'>
+      Publish!
+    </div>
+  </div>
+
+
+
+</main>
+</body>
+</html>
